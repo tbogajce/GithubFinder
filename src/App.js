681d99613fc23c3865ac1e7d22bc676d2fc46fde
Tihomir Bogajcevic <tihomirbogajcevic94@hotmail.com>
2020-05-27 15:53:14 +0200

    Creating class components for User and Navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
-  foo = () => 'Bars';
-
   render() {
-    const name = 'Tihomir Bogajcevic';
-    const loading = false;
-
-    // if (loading) {
-    //   return <h4>Loading...</h4>;
-    // }
-
-    const showName = true;
-
     return (
       <div className='App'>
-        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+        <Navbar title='Github Finder' icon='fab fa-github' />
+        <div className='container'>
+          <Users />
+        </div>
       </div>
     );
   }

f7b085ecfdf6becf5e5b912a0abe6d2759fe248b
Tihomir Bogajcevic <tihomirbogajcevic94@hotmail.com>
2020-05-27 17:01:23 +0200

    Adding Spinner component and refactorin Users to functional component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  // lifecycle method useful when we want to for example make HTTP request (in our case to github)
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github' />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );

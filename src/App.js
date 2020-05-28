9a929e5d9c68dc5e6b439b1f75d560a6448fa617
Tihomir Bogajcevic <tihomirbogajcevic94@hotmail.com>
2020-05-28 11:23:17 +0200

    Added Search component for searching Github users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
 
@@ -17,18 +18,36 @@ class App extends Component {
   };
 
   // lifecycle method useful when we want to for example make HTTP request (in our case to github)
-  async componentDidMount() {
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //   const res = await github.get('/users'); // https://developer.github.com/v3/users/#get-all-users
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  // Search Github users - https://developer.github.com/v3/search/#search-users
+  searchUsers = async (text) => {
     this.setState({ loading: true });
-    const res = await github.get('/users');
-    this.setState({ users: res.data, loading: false });
-  }
+    const res = await github.get(`/search/users?q=${text}`);
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
 
   render() {
+    const { users, loading } = this.state;
+
     return (
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github' />
         <div className='container'>
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
